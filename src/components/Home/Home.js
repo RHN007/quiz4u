f8c6e029bbd4cfe309bcaf4906e8241bd9b8c500
RHN007 <rakibul.nayon@outlook.com>
2022-10-11 10:11:14 +0600

    justify pic

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,11 +3,11 @@ import img from '../../images/worker-placing-blue-cube.jpg'
 
 const Home = () => {
     return (
-        <div className='flex mt-10 justify-between content-center '>
+        <div className='md:flex mt-10 md:justify-between md:content-center '>
             <div className='p-24'> 
-            <h2 className='text-white' >Show real <br /> <span className='text-white text-6xl font-bold'>Power of Your Mind</span></h2>
+            <h2 className='text-white' >Show real <br /> <span className='text-white text-8xl md:text-6xl font-bold'>Power of Your Mind</span></h2>
             </div>
-            <div>
+            <div className='ml-10 md:ml-0'>
                 <img className='rounded' src={img} width={600} alt="" />
             </div>
         </div>
