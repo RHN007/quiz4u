86d473e4f136d2e178fb89b5a7c6ed18b1a7e184
RHN007 <rakibul.nayon@outlook.com>
2022-10-11 10:02:03 +0600

    adding file

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import img from '../../images/worker-placing-blue-cube.jpg'
 
 const Home = () => {
     return (
-        <div>
-            <h1>This is home</h1>
+        <div className='flex mt-10 justify-between content-center '>
+            <div className='p-24'> 
+            <h2 className='text-white' >Show real <br /> <span className='text-white text-6xl font-bold'>Power of Your Mind</span></h2>
+            </div>
+            <div>
+                <img className='rounded' src={img} width={600} alt="" />
+            </div>
         </div>
     );
 };
