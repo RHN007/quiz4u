65991525fb94e673ece5ef52517a7455998472a5
RHN007 <rakibul.nayon@outlook.com>
2022-10-12 19:57:21 +0600

    adding blogs

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -3,7 +3,94 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div>
-            <h1>Blogs for you</h1>
+            <section className="text-gray-600 body-font">
+                <div className="container px-5 py-24 mx-auto">
+                    <div className="flex flex-wrap -m-4">
+                        <div className="p-4 md:w-1/3">
+                            <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+
+                                <div className="p-6">
+                                    <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">Blogs</h2>
+                                    <h1 className="title-font text-lg font-medium text-gray-900 mb-3">What is purpose of react router ?</h1>
+                                    <p className="leading-relaxed mb-3">React Router is a standard library for routing in React. It enables the navigation among views of various components in a React Application, allows changing the browser URL, and keeps the UI in sync with the URL. <br />
+                                    It has a API with Powerful features like code loading , dynamic route matching and location transition handling build right in. <br />
+                                    React Routing saves a lot of time for coding. </p>
+                                    <div className="flex items-center flex-wrap ">
+                                    
+                                        <span className="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                                            <svg className="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                                                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                                                <circle cx="12" cy="12" r="3"></circle>
+                                            </svg>1.2K
+                                        </span>
+                                        <span className="text-gray-400 inline-flex items-center leading-none text-sm">
+                                            <svg className="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                                                <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                                            </svg>6
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="p-4 md:w-1/3">
+                            <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+
+                                <div className="p-6">
+                                    <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">BLOGS</h2>
+                                    <h1 className="title-font text-lg font-medium text-gray-900 mb-3">How does context api works ?</h1>
+                                    <p className="leading-relaxed mb-3">React.createContext() is all you need. It returns a consumer and a provider. Provider is a component that as it's names suggests provides the state to its children. It will hold the "store" and be the parent of all the components that might need that store. Consumer as it so happens is a component that consumes and uses the state. <br />
+                                    It effectively produce global variables that can be passed around . This is alternative of "Props drilling".</p>
+                                    <div className="flex items-center flex-wrap">
+                                      
+                                        <span className="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                                            <svg className="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                                                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                                                <circle cx="12" cy="12" r="3"></circle>
+                                            </svg>1.2K
+                                        </span>
+                                        <span className="text-gray-400 inline-flex items-center leading-none text-sm">
+                                            <svg className="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                                                <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                                            </svg>6
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="p-4 md:w-1/3">
+                            <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+
+                                <div className="p-6">
+                                    <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">BLOGS</h2>
+                                    <h1 className="title-font text-lg font-medium text-gray-900 mb-3">What is Refs in React?</h1>
+                                    <p className="leading-relaxed mb-3">Refs provide a way to access DOM nodes or React elements created in the render method. <br />
+                                   <span className='font-bold'>When to use Ref:</span>
+                                   <br />
+                                   Managing focus, text selection, or media playback, triggering imperative animations & Integrating with third party. <br />
+                                   Refs are created using React.createRef() and attached to React elements via the ref attribute. <br />
+                                   Refs can be use for a dom element, to a class components. 
+                                   React also supports another way to set refs called “callback refs”, which gives more fine-grain control over when refs are set and unset.
+</p>                            
+                                    <div className="flex items-center flex-wrap ">
+                                   
+                                        <span className="text-gray-400 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                                            <svg className="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                                                <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+                                                <circle cx="12" cy="12" r="3"></circle>
+                                            </svg>1.2K
+                                        </span>
+                                        <span className="text-gray-400 inline-flex items-center leading-none text-sm">
+                                            <svg className="w-4 h-4 mr-1" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24">
+                                                <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                                            </svg>6
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
     );
 };
