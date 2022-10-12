49e3df52d91e7c17f7a5ca4eae448b759dc11744
RHN007 <rakibul.nayon@outlook.com>
2022-10-12 08:47:01 +0600

    desing changing

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,41 +1,42 @@
 import React, { useState } from 'react';
-import {  EyeIcon, EyeSlashIcon } from '@heroicons/react/24/solid'
+import {   EyeSlashIcon } from '@heroicons/react/24/solid'
 
 const Question = ({ qts, index }) => {
     const {  options, question,correctAnswer } = qts;
     const [open, setOpen] = useState(false)
     return (
         <div>
-            <div className="quiz border border-2 border-green-700 rounded-lg   mt-5 p-5">
-              <div className='flex justify-between'>
+            <div className="quiz  outline outline-offset-2  outline-green-700 rounded-lg  m-5 md:m-0 md:mt-5 p-5">
+              <div className='  md:flex md:justify-between'>
               <h1 className=' mb-4 font-bold text-blue-500'>Quiz:{index + 1}  {question}</h1>
-                <div>
+                <div className='text-center'>
                    {
-                    open ?  <span>{correctAnswer}</span>  : <EyeSlashIcon onClick={() => setOpen(!open)} className ='h-6 w-6 text-blue-500'/>
+                    open ?  <span> <small className='font-bold text-lg text-green-600'>Answer:</small> {correctAnswer}</span>  : <EyeSlashIcon onClick={() => setOpen(!open)} className ='h-6 w-6 text-blue-500'/>
                    }
                   
                 </div>
               </div>
                 <div className='p-5'>
-                    <div className='grid grid-cols-2 gap-3'>
-                        <div>
-                            <label className="">
+                    <div className='grid grid-cols-1 md:grid-cols-2 gap-3'>
+                        <div className='border border-green-700 p-4 rounded-lg bg-slate-100'>
+                            <label >
                                 <input name="radio" type="radio"  />
                                 <span className='ml-2'>{options[0]}</span>
                             </label>
                         </div>
-                        <div> <label className="">
+                        <div className='border border-green-700 p-4 rounded-lg bg-slate-100'>
+                             <label>
                             <input name="radio" type="radio" />
                             <span className='ml-2'>{options[1]}</span>
                         </label></div>
-                        <div>
-                            <label className="">
+                        <div className='border border-green-700 p-4 rounded-lg bg-slate-100'>
+                            <label>
                                 <input name="radio" type="radio"  />
                                 <span className='ml-2'>{options[2]}</span>
                             </label>
                         </div>
-                        <div>
-                            <label className="">
+                        <div className='border border-green-700 p-4 rounded-lg bg-slate-100'>
+                            <label>
                                 <input name="radio" type="radio"  />
                                 <span className='ml-2'>{options[3]}</span>
                             </label>
--- src/components/TopicDetails/TopicDetails.js
+++ src/components/TopicDetails/TopicDetails.js
@@ -6,10 +6,10 @@ import Question from '../Question/Question';
 const TopicDetails = () => {
     const details = useLoaderData()
     console.log(details.data)
-    const {id, name, questions} = details.data
+    const { name, questions} = details.data
     return (
         <div>
-            <h1 className='text-center'>Questions of {name}</h1>
+            <h1 className='text-center font-bold text-xl md:text-3xl mt-5 text-green-800'>Questions of {name}</h1>
            <div>
            
             {questions.map((q, index)=> <Question key={q.id} index={index} qts={q}></Question>)}
